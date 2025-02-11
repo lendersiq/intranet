4fb20d8f478cdd02e1ce17798b48ea4bb343974c
lendersiq <lendersiq@gmail.com>
2025-02-10 21:30:23 -0500

    Add files via upload

---
+++ js/bookmarks.js
@@ -0,0 +1,9 @@
+// googleBookmarks keyed by "bookmark_key"
+// e.g. googleBookmarks["vacation"] => "https://docs.google.com/..."
+const googleBookmarks = {
+  "vacation": "https://docs.google.com/document/d/5678/edit#bookmark=id.vacation",
+  "loan": "https://docs.google.com/document/d/5678/edit#bookmark=id.loans",
+  "mortgage": "https://docs.google.com/document/d/ABCD/edit#bookmark=id.mortgage",
+  "engagement": "https://docs.google.com/document/d/XYZ/edit#bookmark=id.hr",
+  // etc.
+};
